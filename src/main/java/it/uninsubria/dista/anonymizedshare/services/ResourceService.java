f21935cb4ad1a0edd559d647df87b0c1fcaad87e
Albertini <albertini.da@gmail.com>
2013-07-11 18:02:38 +0200

    some tag add in ResourceService

--- src/main/java/it/uninsubria/dista/anonymizedshare/services/ResourceService.java
+++ src/main/java/it/uninsubria/dista/anonymizedshare/services/ResourceService.java
@@ -1,7 +1,12 @@
 package it.uninsubria.dista.anonymizedshare.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import it.uninsubria.dista.anonymizedshare.services.interfaces.ResourceServiceInterface;
 
+@Service
+@Transactional
 public class ResourceService implements ResourceServiceInterface {
 
 }
