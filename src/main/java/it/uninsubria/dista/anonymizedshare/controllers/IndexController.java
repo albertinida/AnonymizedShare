c14e07b119bc819c32e2e31ecda6c995ce861e99
Albertini <albertini.da@gmail.com>
2013-07-12 00:02:15 +0200

    Test communication with KeyManager

--- src/main/java/it/uninsubria/dista/anonymizedshare/controllers/IndexController.java
+++ src/main/java/it/uninsubria/dista/anonymizedshare/controllers/IndexController.java
@@ -1,5 +1,14 @@
 package it.uninsubria.dista.anonymizedshare.controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +16,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	@Value("${architecture.keymanager}")
+	private String keyManagerAddress;
+	
 	@RequestMapping(value = "/", produces = "text/html")
-	public String index(Model uiModel) {
+	public String index(Model uiModel) throws MalformedURLException, IOException {
 
-		uiModel.addAttribute("message", "test message");
+		HttpURLConnection keyManagerConnection;
+		URL url = new URL(keyManagerAddress);
+		keyManagerConnection = (HttpURLConnection) url.openConnection();
+		keyManagerConnection.setRequestMethod("POST");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(keyManagerConnection.getInputStream()));
+		String jsonResponse = "", jsonLine = "";
+		
+		while ((jsonLine = br.readLine()) != null) 
+		 jsonResponse += jsonLine;
+		br.close();
+		
+		uiModel.addAttribute("message", jsonResponse);
 		return "index";
 	}
 }
